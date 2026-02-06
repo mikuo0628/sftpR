5643e288c835f035c206ee1a008af45c77370e99
Kuo <michael.kuo@bccdc.ca>
2026-02-05 19:04:36 -0800

    Init download function

---
+++ R/sftp_download.R
@@ -0,0 +1,83 @@
+sftp_download <- function(
+    sftp_conn = NULL,
+    path_file = NA_character_,
+    path_save = NA_character_,
+    .verbose  = T,
+    ...
+) {
+
+  verbose_msg <- function(msg, .print = .verbose) { if (.print) message(msg) }
+
+  browser()
+  # cases:
+  # 0 path_file, 1 path_save: generate path_save
+  # 0 path_file, 0 path_save: generate path_save
+  # 0 path_file, n path_save: error
+  # 1 path_file, 1 path_save: generate path_save
+  # 1 path_file, 0 path_save: generate path_save
+  # 1 path_file, n path_save: error
+  # n path_file, 0 path_save: generate path_save
+  # n path_file, 1 path_save: generate path_save
+  # n path_file, n path_save: 1-to-1
+  # n path_file, m path_save: error
+
+  # set up errors
+  err_msgs <-
+    c(
+      "More `path_save` that can be logically assigned downloadable files.",
+      "No downloadable files found."
+    )
+
+  if (is.na(path_file) & length(path_save) > 0)       { stop(err_msgs[1]) }
+  if (length(path_file) == 1 & length(path_save) > 0) { stop(err_msgs[1]) }
+  if (length(path_file) != length(path_save))         { stop(err_msgs[1]) }
+
+  if (is.na(path_file) || path_file == '*') {
+
+    # list and download all files if NA or wildcard
+    df_files <- subset(sftp_list(sftp_conn, .recursive = T), type == 'file')
+
+  }
+
+  if (is.na(path_save)) path_save <- getwd()
+
+  file.info(
+    c(
+      path_save,
+      file.path(path_save, 'test.txt')
+    )
+  )$isdir
+
+  df_files$path_save <- path_save
+
+  # TODO: check if path_save is file, path_save and path_file are equal length
+  # TODO: iterate downloads
+
+  file.info(path_save)$isdir
+  file.exists(path_save)
+  dir.exists(path_save)
+  dir.exists(file.path(path_save, 'test.txt'))
+
+  curl::curl_download(
+    url = file.path(gsub('/$', '', df_files$url), df_files$name)[1],
+    destfile = file.path(path_save, df_files$name[1]),
+    quiet = F,
+    handle = sftp_conn$h
+  )
+
+
+}
+
+sftp_download(
+  sftp_conn =
+    sftp_connect(
+      server   = 'sftp://sftp.phsa.ca/',
+      # folder   = '/////test///test////',
+      # username = 'SVC_BCCDCAnalytics',
+      # password = 'y26W99322-84294',
+      # folder   = 'BCEHS',
+      username = 'michael.kuo@bccdc.ca',
+      password = keyring::key_get('michael.kuo'),
+      port     = 22
+    )
+)
