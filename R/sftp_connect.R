856c41274aa1f957a0b5d1e2c4d83e51fee0ac03
Michael Kuo <michael.kuo@bccdc.ca>
2026-02-08 07:17:37 +0000

    Add doc

--- R/sftp_connect.R
+++ R/sftp_connect.R
@@ -1,11 +1,25 @@
 #' Creates a list object of connection parameters.
 #'
-#' @param server single element character vector of server URL or IP address.
-#'   Defaults to "localhost". Will clean character of any forward slashes and
-#'   replace protocol designation with `protocol`.
+#' This function creates a list object of connection parameters,
+#' including an SFTP handle, to be used in other `sftpR` SFTP related functions.
+#' It will also check if the `hostname` is valid and can be found,
+#' and will warn if a port or folder path is included in the `hostname`
+#' and override the corresponding arguments.
+#' The function will sanitize the `hostname` and `folder` of minor
+#' formatting issues, such as extra slashes.
+#'
+#' @param hostname single element character vector of hostname URL or
+#'   IP address. Defaults to "localhost". The funciton will sanitize
+#'   `hostname` of minor formatting issues, such as extra slashes.
+#'   If a port is included in the `hostname`, it will warn and override
+#'   the `port` argument.
+#'   If a folder path is included in the `hostname`, it will warn and override
+#'   the `folder` argument.
 #' @param folder single element character vector of the folder sub-path. Can
 #'   be multiple sub levels, ie. `dir_1/dir_2/dir_3`. Case-sensitive.
 #'   Defaults to `NULL`, which directs to root folder.
+#'   The function will sanitize `folder` of minor formatting issues,
+#'   such as extra slashes.
 #' @param username single element character vector of SFTP account name.
 #'   Recommend using `keyring`package or system variables to manage.
 #'   Defaults to `NULL`.
@@ -20,11 +34,23 @@
 #' @param timeout single element integer vector. `curl` connection timeout value
 #'   in seconds. Defaults to `30`.
 #'
-#' @returns list object of values to be used in other `sftpR` SFTP related
-#'   functions.
+#' @returns list object of values and handle, to be used in other `sftpR`
+#'   SFTP related functions.
 #' @export
 #'
 #' @examples
+#' \dontrun{
+#' # Quick connectivity test
+#' # Establish a connection
+#' conn <- sftp_connect(
+#'   hostname = "sftp://127.0.0.1:2222/upload",
+#'   user = "tester",
+#'   password = "password123",
+#'   timeout = 30,
+#'   .verbose = TRUE
+#' )
+#' }
+#' @export
 sftp_connect <- function(
     hostname = "localhost",
     folder = NULL,
@@ -34,7 +60,7 @@ sftp_connect <- function(
     port = 22L,
     timeout = 30L,
     .verbose = FALSE) {
-# check protocol "sftp"
+  # check protocol "sftp"
   sftp_check <- "sftp" %in% curl::curl_version()$protocol
   if (isFALSE(sftp_check)) stop("Please check if `curl` contains `sftp` protocol")
 
@@ -42,7 +68,7 @@ sftp_connect <- function(
   h <- curl::new_handle()
   curl::handle_setopt(
     h,
-userpwd = paste0(username, ":", password),
+    userpwd = paste0(username, ":", password),
     ssh_auth_types = 2,
     verbose = .verbose
   )
@@ -60,13 +86,13 @@ userpwd = paste0(username, ":", password),
   }
 
   sftp_conn <-
-          list(
-        protocol = protocol,
-        hostname = hostname,
-        port     = port,
-        h        = h,
-                timeout  = timeout
-          )
+    list(
+      protocol = protocol,
+      hostname = hostname,
+      port     = port,
+      h        = h,
+      timeout  = timeout
+    )
 
   return(sftp_conn)
 }
