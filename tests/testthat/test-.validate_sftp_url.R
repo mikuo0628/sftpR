5742dac22372d619884294eaab6a66591442117b
Michael Kuo <mikuo0628@gmail.com>
2026-03-01 21:04:50 +0000

    Init unit test

---
+++ tests/testthat/test-.validate_sftp_url.R
@@ -0,0 +1,23 @@
+test_that(
+  "SFTP URLs are validated",
+  {
+    sftp_conn <-
+      sftp_connect$new(
+        hostname = "sftp://127.0.0.1:2222/",
+        username = "tester",
+        password = "password123"
+      )
+    correct_url <- "sftp://127.0.0.1:2222/upload/mtcars.csv"
+    bad_url_1   <- "sftp:/127.0.0.1:2222/upload/mtcars.csv"
+    bad_url_2   <- "127.0.0.1:2222/upload/mtcars.csv"
+    bad_url_3   <- "127.0.0.1/upload/mtcars.csv"
+    bad_url_4   <- "/upload/mtcars.csv"
+    bad_url_5   <- "upload/mtcars.csv"
+
+    expect_equal(.validate_sftp_url(sftp_conn, bad_url_1), correct_url)
+    expect_equal(.validate_sftp_url(sftp_conn, bad_url_2), correct_url)
+    expect_equal(.validate_sftp_url(sftp_conn, bad_url_3), correct_url)
+    expect_equal(.validate_sftp_url(sftp_conn, bad_url_4), correct_url)
+    expect_equal(.validate_sftp_url(sftp_conn, bad_url_5), correct_url)
+  }
+)
