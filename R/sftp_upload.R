cb718e1422b16329177aef614af6952eb063f275
Michael Kuo <mikuo0628+github@gmail.com>
2026-03-06 20:30:02 -0800

    Reorder on.exit to after appropriate variable assignment - return invisible TRUE

--- R/sftp_upload.R
+++ R/sftp_upload.R
@@ -4,11 +4,6 @@ sftp_upload <- function(
     remote_file = NULL,
     .create_dir = FALSE,
     .verbose = TRUE) {
-  on.exit(close(upload_h$file_con), add = TRUE)
-  on.exit(
-    if (!is.null(upload_h$tempfile)) unlink(upload_h$tempfile),
-    add = TRUE
-  )
   # 1) Sanitize and check `remote_file`: should be relative
   remote_file <- 
     .validate_sftp_url(sftp_conn = sftp_conn, remote_file,  .verbose = .verbose)
@@ -23,6 +18,12 @@ sftp_upload <- function(
       ftp_create_missing_dirs = ifelse(isTRUE(.create_dir), 1L, 0L),
       .verbose = .verbose
     )
+
+  on.exit(close(upload_h$file_con), add = TRUE)
+  on.exit(
+    if (!is.null(upload_h$tempfile)) unlink(upload_h$tempfile),
+    add = TRUE
+  )
 
   .verbose_msg(
     .verbose = .verbose,
@@ -40,4 +41,6 @@ sftp_upload <- function(
   } else {
     .verbose_msg(.verbose = .verbose, "Upload successful!", message)
   }
+
+  return(invisible(TRUE))
 }
