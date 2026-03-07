09b26d973e6736d7f374786d7f3d9d12a25494da
Michael Kuo <mikuo0628+github@gmail.com>
2026-03-06 18:23:36 -0800

    Refactor reusable parts of sftp_upload - sanitize using .validate_sftp_url - more controllable .verbose - cleaner verbose msgs

--- R/sftp_upload.R
+++ R/sftp_upload.R
@@ -9,39 +9,35 @@ sftp_upload <- function(
     if (!is.null(upload_h$tempfile)) unlink(upload_h$tempfile),
     add = TRUE
   )
-  browser()
-  # 0) 
-  sftp_conn$clean_url
   # 1) Sanitize and check `remote_file`: should be relative
-  file.path(
-    sftp_conn$clean_url$full_url,
-    remote_file
-  )
-
-  gsub("(.*://)", "", "sftp://127.0.0.1", perl = TRUE)
+  remote_file <- 
+    .validate_sftp_url(sftp_conn = sftp_conn, remote_file,  .verbose = .verbose)
 
   # 2) create upload handle
   # - this step creates curl handle specifically for upload
   # - and checks local file
-  sftp_list(sftp_conn, .recursive = TRUE)
   upload_h <-
     sftp_conn$.upload_handle(
-      local_file = local_file, reuse = TRUE,
-      ftp_create_missing_dirs = ifelse(isTRUE(.create_dir), 1L, 0L)
+      local_file = local_file,
+      reuse = TRUE,
+      ftp_create_missing_dirs = ifelse(isTRUE(.create_dir), 1L, 0L),
+      .verbose = .verbose
     )
 
-  try_upload <-
-    try(
-      curl::curl_fetch_memory(
-        "sftp://127.0.0.1:2222/upload/subdir/sub2/mtcars3.csv",
-        handle = upload_h$h
-      )
-    )
+  .verbose_msg(
+    .verbose = .verbose,
+    sprintf(
+      "\nUploading: \n[%s] ===> [%s]\n",
+      upload_h$tempfile, remote_file
+    ),
+    message
+  )
 
-  grep("Remote file not found", try_upload[[1]])
+  try_upload <- try(curl::curl_fetch_memory(remote_file, handle = upload_h$h))
+
   if (inherits(try_upload, "try-error")) {
-    stop("Upload failed: ", conditionMessage(attr(try_upload, "condition")))
+    stop("!! Upload failed !!: ", conditionMessage(attr(try_upload, "condition")))
   } else {
-    message("Upload successful!")
+    .verbose_msg(.verbose = .verbose, "Upload successful!", message)
   }
 }
